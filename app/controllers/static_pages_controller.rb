d420001ff32133fe57b6b40a2e5d37f72ffc0d10
Javier Hermosillo <javierhermosillo@MacBook-Pro-de-Javier.local>
2016-02-11 18:41:27 -0600

    inicial

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about_me
+  	@files = Dir.glob('*')
+  end
+
+  def surprise
+  	 number = params[:limit] || 10
+  	@size = (1..number.to_i).to_a
+  end
+
+  def help
+  end
+end
